26be0c8e135ba73056692c1680db35b572146b5a
Anindya Sekar Paramitha <anindyasekarp@gmail.com>
2023-12-10 14:54:45 +0700

    Tambah Jumlah Peminjaman Berdasarkan Fakultas Peminjam per Bulan

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,9 @@ def create_borrow_duration_df(df):
   pass
 
 def create_borrower_by_faculty_per_month_df(df):
-  pass
+  pinjaman_bulanan_by_fakultas = df.groupby([pd.Grouper(key='Tgl Pinjam', freq='M'), 'Fakultas Pemustaka'])['Nomor Buku'].count().unstack(fill_value=0)
+  pinjaman_bulanan_by_fakultas.index = pinjaman_bulanan_by_fakultas.index.month_name()
+  return pinjaman_bulanan_by_fakultas.reset_index().melt(id_vars='Tgl Pinjam', var_name='Fakultas', value_name='Banyak Pinjaman')
 
 def create_transactions_df(df):
   pass
@@ -19,11 +21,27 @@ def create_transactions_df(df):
 ### MAIN PROGRAM
 df_pinjaman = pd.read_csv('daftar-peminjaman-2022.csv')
 
+# Ubah tipe data kolom Tgl Pinjam dan Tgl Kembali menjadi datetime untuk visualisasi
+kolom_tgl = ['Tgl Pinjam', 'Tgl Kembali']
+
+for kolom in kolom_tgl:
+    df_pinjaman[kolom] = pd.to_datetime(df_pinjaman[kolom], format='%Y-%m-%d', errors='coerce')
+
 # Visualisasi jumlah pinjaman berdasarkan tipe koleksi per bulan
 
 # Visualisasi durasi pinjaman
 
 # Visualisasi jumlah pinjaman berdasarkan fakultas peminjam per bulan
+df_borrower_by_faculty_per_month = create_borrower_by_faculty_per_month_df(df_pinjaman)
+
+fig = plt.figure(figsize=(12, 5))
+sns.barplot(data=df_borrower_by_faculty_per_month, x='Tgl Pinjam', y='Banyak Pinjaman', hue='Fakultas', palette='bright')
+plt.title('Banyak Pinjaman Berdasarkan Fakultas')
+plt.xlabel('Bulan Peminjaman')
+plt.xticks(rotation=0)
+
+st.subheader('Jumlah Peminjaman Berdasarkan Fakultas Peminjam per Bulan')
+st.pyplot(fig)
 
 # Analisis Asosiasi
 support = st.slider(
